9f6d533d8d8b3113758f19891d8d45299c75d4a7
autodecode <hiddenhorizons51@icloud.com>
2026-09-16 14:24:48 +0300

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,43 @@
+(() => {
+let lastUrl=location.href;
 
+function checkUrl(){
+const url=location.href;
+
+if(url===lastUrl)return;
+lastUrl=url;
+chrome.runtime.sendMessage({
+  type:"checkUrl",
+  url
+}).then(result=>{
+  if(!result)return;
+  const target=chrome.runtime.getURL(
+    `blocked.html?category=${encodeURIComponent(result.category)}&host=${encodeURIComponent(result.host)}&url=${encodeURIComponent(result.url)}&mode=${encodeURIComponent(result.mode)}`
+  );
+  if(location.href!==target){
+    location.replace(target);
+  }
+}).catch(()=>{});
+
+}
+
+const originalPushState=history.pushState;
+const originalReplaceState=history.replaceState;
+
+history.pushState=function(…args){
+const result=originalPushState.apply(this,args);
+checkUrl();
+return result;
+};
+
+history.replaceState=function(…args){
+const result=originalReplaceState.apply(this,args);
+checkUrl();
+return result;
+};
+
+window.addEventListener(“popstate”,checkUrl);
+window.addEventListener(“hashchange”,checkUrl);
+
+setInterval(checkUrl,1000);
+})();
